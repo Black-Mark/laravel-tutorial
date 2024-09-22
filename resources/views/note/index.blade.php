572ba19d8f087c9e4dfd2bac1c5065ca88b59475
Black-Mark <jeshuabay@gmail.com>
2024-09-23 00:34:52 +0800

    new file:   app/Http/Controllers/NoteController.php 	modified:   app/Http/Controllers/WelcomeController.php 	new file:   app/Models/Note.php 	new file:   database/factories/NoteFactory.php 	new file:   database/migrations/2024_09_22_145416_create_notes_table.php 	modified:   database/seeders/DatabaseSeeder.php 	modified:   resources/css/app.css 	new file:   resources/views/components/layout.blade.php 	new file:   resources/views/note/create.blade.php 	new file:   resources/views/note/edit.blade.php 	new file:   resources/views/note/index.blade.php 	new file:   resources/views/note/show.blade.php 	modified:   routes/web.php

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,28 @@
+<x-layout>
+
+    <h1>Index</h1>
+
+    <div class="note-container single-note">
+        <a href="{{ route('note.create') }}" class="new-note-btn">
+            New Note
+        </a>
+    </div>
+
+    <div class="notes">
+
+        @foreach ($notes as $note)
+        <div class="note">
+            <div class="note-body">
+                {{ Str::words($note->note, 30) }}
+            </div>
+            <div class="note-buttons">
+                <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
+                <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                <button class="note-delete-button">Delete</button>
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+
+</x-layout>
